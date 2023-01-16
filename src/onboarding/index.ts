3cca5fc7cfbaeb6540081e461e1639a18a8eece9
NelsonPhilippe <philippenelson59@gmail.com>
2023-01-16 21:07:35 +0100

    build(common) : delete env file and add in gitignore

--- src/onboarding/index.ts
+++ src/onboarding/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Client, Events, GatewayIntentBits} from "discord.js";
 
 import * as dotenv from "dotenv";
@@ -11,6 +10,3 @@ console.log(DISCORD_TOKEN);
 const client = new Client({ intents : [GatewayIntentBits.Guilds]});
 
 client.login(DISCORD_TOKEN);
-=======
-console.log('test');
->>>>>>> 5ae2a1e (build(bot) : add run command)
