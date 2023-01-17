ee3ea1f280462e6a11c4ed094ccab2c7ac2ccce0
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-01-17 15:17:05 +0100

    feat(signature): update index.js, add the /commands 'active'

--- src/signature/index.js
+++ src/signature/index.js
@@ -1,30 +1,20 @@
 import {
-  Client, Collection, Events, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle,
+  Client, Events, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle,
 } from 'discord.js';
-import fs from 'node:fs';
-import path from 'node:path';
+import commands_handler from './handlers/deploy-comands.js'
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const { DISCORD_TOKEN } = process.env;
-const { DISCORD_ID } = process.env;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-
-const commandFiles = fs.readdirSync('./signature/commands').filter((file) => file.endsWith('.js'));
-
-(async () => { for (const filePath of commandFiles) {
-  const command = await import(`./commands/${filePath}`);
-  client.commands.set(command.default.data.name, command.default);
-}})();
+commands_handler(client, DISCORD_TOKEN, CLIENT_ID);
 
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
-client.login(DISCORD_TOKEN);
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
@@ -41,3 +31,5 @@ client.on(Events.InteractionCreate, async (interaction) => {
     await interaction.reply({ content: 'Click the button to active me!', components: [row] });
   }
 });
+
+client.login(DISCORD_TOKEN);
