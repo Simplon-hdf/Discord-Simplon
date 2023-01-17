eba97ec6e0fe0c257b4fc5940df94468d7da4ad9
NelsonPhilippe <philippenelson59@gmail.com>
2023-01-17 03:00:50 +0100

    feat(onboarding) fix commands handling

--- src/onboarding/handlers/commands_handler.ts
+++ src/onboarding/handlers/commands_handler.ts
@@ -1,42 +1,36 @@
-import { REST, Routes, Collection } from "discord.js";
-import fs from "node:fs";
+/// <reference path="../commands/ping_cmd.ts"/>
 
+import { REST, Routes, Collection, SlashCommandBuilder, Client } from "discord.js";
+import * as fs from "fs";
 import * as dotenv from "dotenv";
 
-export default async (client, discord_token, discord_client_id) => {
-    client.handleCommands = async () => {
-        dotenv.config();
+export default async (client : Client, discord_token, discord_client_id) => {
+    dotenv.config();
+    
+    const commandFiles = fs.readdirSync('./onboarding/dist/commands/');
+    const commands : SlashCommandBuilder[] = [];
 
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-        client.commands = new Collection();
-
-        for (const file of commandFiles) {
-            console.log(file);
-            const cmd = await import(`../commands/${file}`);
-
-            if ('data' in cmd && 'execute' in cmd) {
-                client.commands.set(cmd.data.name, cmd);
-            } else {
-                console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
-            }
-        }
-
-        
-
-        const rest = new REST({ version: '10' }).setToken(discord_token);
-
-        (async () => {
-            try {
-                console.log('Started refreshing application (/) commands.');
-
-                await rest.put(Routes.applicationCommands(discord_client_id), { body: client.commands });
-
-                console.log('Successfully reloaded application (/) commands.');
-            } catch (error) {
-                console.error(error);
-            }
-        })();
+    for (const file of commandFiles) {
+        // console.log(file);
+        const cmd = await import(`../commands/${file}`);
+        commands.push(cmd.default.data);
+        console.log(commands);
 
     }
+
+
+
+    const rest = new REST({ version: '10' }).setToken(discord_token);
+
+    (async () => {
+        try {
+            console.log('Started refreshing application (/) commands.');
+
+            await rest.put(Routes.applicationCommands(discord_client_id), { body: commands});
+
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    })();
 }
