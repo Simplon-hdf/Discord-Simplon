25cb706120e0da26554421accaf6be4012a7ec9c
Mecopi <mecopilive@gmail.com>
2023-01-19 10:58:42 +0100

    update(architecture): remove reliquas events

--- src/onboarding/events/reject-id-req.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonInteraction, Events } from "discord.js";
-
-export default {
-    name : Events.InteractionCreate,
-    on: true,
-    execute(interaction: ButtonInteraction) {
-        if(!interaction.isButton() || interaction.customId != 'reject-id-req') return;
-    }
-}
