8419683789076c4277c01579327167f49803e94d
Mecopi <mecopilive@gmail.com>
2023-01-19 10:32:11 +0100

    feat(identification): add way for CAPS to reject id request

--- src/onboarding/events/reject-id-req.ts
+++ src/onboarding/events/reject-id-req.ts
@@ -1,10 +1,9 @@
 import { ButtonInteraction, Events } from "discord.js";
 
 export default {
-    name: Events.InteractionCreate,
+    name : Events.InteractionCreate,
     on: true,
-    async execute(interaction: ButtonInteraction) {
-        if (!interaction.isModalSubmit() || interaction['customId'] != 'reject-id-req') return;
-            
+    execute(interaction: ButtonInteraction) {
+        if(!interaction.isButton() || interaction.customId != 'reject-id-req') return;
     }
 }
