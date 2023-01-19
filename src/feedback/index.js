95883f872819bf36250785b64c140ef0b5613182
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-01-19 12:09:13 +0100

    feat(feedback): work on the selection function

--- src/feedback/index.js
+++ src/feedback/index.js
@@ -7,15 +7,23 @@ import {
   ButtonStyle,
   StringSelectMenuBuilder,
   EmbedBuilder,
-} from "discord.js";
-import commands_handler from "./handlers/deploy-comands.js";
-import * as dotenv from "dotenv";
-// import database from "./database/db.json" assert { type: "json" };
+} from 'discord.js';
+import commands_handler from './handlers/deploy-comands.js';
+import * as dotenv from 'dotenv';
+import database from './database/db.json' assert { type: 'json' };
 
 dotenv.config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildInvites] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildInvites,
+  ],
+});
 
 commands_handler(client, DISCORD_TOKEN, CLIENT_ID);
 
@@ -24,13 +32,17 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-  if(interaction.isChatInputCommand()){
-    if(interaction.commandName === "setup-feedback"){
+  if (interaction.isChatInputCommand()) {
+    if (interaction.commandName === 'setup-feedback') {
       const configEmbed = new EmbedBuilder()
         .setColor(0x0099ff)
         .setTitle('Commencer la procédure de configuration de feedback')
-        .setDescription(`Bonjour ${interaction.member.displayName}, \n\n pour commencer la procédure de configuration de feedback, veuillez cliquer sur un des boutons ci-dessous.`)
-        .setThumbnail('https://cdn-icons-png.flaticon.com/512/1087/1087804.png')
+        .setDescription(
+          `Bonjour ${interaction.member.displayName}, \n\n pour commencer la procédure de configuration de feedback, veuillez cliquer sur un des boutons ci-dessous.`
+        )
+        .setThumbnail(
+          'https://cdn-icons-png.flaticon.com/512/1087/1087804.png'
+        );
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
           .setCustomId('sendExistingFeedback')
@@ -47,13 +59,52 @@ client.on(Events.InteractionCreate, async (interaction) => {
           .setLabel('Configurer un modèle de feedback existant')
           .setEmoji('⚙️')
           .setStyle(ButtonStyle.Secondary)
-      )
+      );
       await interaction.reply({
         embeds: [configEmbed],
-        components: [row]
+        components: [row],
       });
-    };
-  };
+    }
+  } else if (interaction.isButton()) {
+    if (interaction.customId === 'sendExistingFeedback') {
+      
+      const optionConstructor = (modal_list) => {
+        for (let modal in modal_list) {
+          return {
+            label: `[${modal.title}]`,
+            description: `${modal.description}`,
+            value: `${modal.customId}`,
+          };
+        }
+      }
+
+      const templateSelectionEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle('Sélection du feedBack')
+        .setDescription(
+          'Veuiller sélectionner le feedback souhaité dans la liste ci-dessous'
+        )
+        .setThumbnail(
+          'https://cdn-icons-png.flaticon.com/512/1087/1087804.png'
+        );
+
+      const row = new ActionRowBuilder().addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId('select_template')
+          .setPlaceholder("Aucune réponse n'est sélectionnée")
+          .setMinValues(1)
+          .setMaxValues(5)
+          .addOptions(optionConstructor(database.modals))     
+      );
+
+      await interaction.reply({
+        embeds: [templateSelectionEmbed],
+        components: [row],
+        ephemeral: true,
+      });
+
+    }
+  }
 });
 
 client.login(DISCORD_TOKEN);
