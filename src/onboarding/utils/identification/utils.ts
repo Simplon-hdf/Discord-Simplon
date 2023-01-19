3b6368ba2970694529a079fd34702f8195bc9109
Mecopi <mecopilive@gmail.com>
2023-01-19 15:13:49 +0100

    feat(identification): add removing identifications request method

--- src/onboarding/utils/identification/utils.ts
+++ src/onboarding/utils/identification/utils.ts
@@ -1,4 +1,5 @@
-import { TextChannel } from "discord.js";
+import { GuildMember, TextChannel } from "discord.js";
+import { redefine } from "../json_utils.js";
 
 export const remove_id_message_interactions = async (interaction, message_id, content) => {
 
@@ -6,4 +7,15 @@ export const remove_id_message_interactions = async (interaction, message_id, co
     const identification_mannagement_message = await channel.messages.fetch(message_id);
     await identification_mannagement_message.edit({content: content, embeds: identification_mannagement_message.embeds, components:[]});
 
+}
+
+export const remove_id_request = async(file_path: string, member : GuildMember, id_requests) => {
+    let new_requests_ids = {};
+
+    for(const request in id_requests) {
+        if(id_requests[request]['user_id'] != member.user.id) {
+            new_requests_ids[request] = id_requests[request];
+        }
+    }
+    redefine(file_path, new_requests_ids);
 }
