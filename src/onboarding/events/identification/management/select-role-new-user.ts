fa7b20590a15c24befdb8b138d5bda95d370720b
Benjamin Gam <100224852+benjGam@users.noreply.github.com>
2023-01-20 20:08:49 +0100

    Feature/update identification management (#110)
    * feat(common): update feat to update message and dont send new messages
    
    * feat(common): update channels ids to feat with Cedric server ids
    
    * feat(common): add enum value
    
    * feat(common): reset config courses db

--- src/onboarding/events/identification/management/select-role-new-user.ts
+++ src/onboarding/events/identification/management/select-role-new-user.ts
@@ -1,4 +1,4 @@
-import { Events, GuildMember, StringSelectMenuInteraction } from 'discord.js'
+import { Events, GuildMember, InteractionResponse, StringSelectMenuInteraction } from 'discord.js'
 import { get } from '../../../utils/json_utils.js'
 import { remove_id_message_interactions, remove_id_request } from '../../../utils/identification/utils.js'
 import { RolesIds } from '../../../utils/enums.js'
@@ -8,23 +8,32 @@ export default {
     on: true,
     used_file: './identifications-requests.json',
     async execute(interaction: StringSelectMenuInteraction) {
-        if(interaction.guild == null) return;
-        if(!interaction.isStringSelectMenu() || interaction.customId != 'attributed-role-id') return;
+        if (interaction.guild == null) return;
+        if (!interaction.isStringSelectMenu() || interaction.customId != 'attributed-role-id') return;
+        
         const role_id = interaction.values[0].split('|')[0];
         const message_id = interaction.values[0].split('|')[1];
         const id_requests = await get(this.used_file);
         const id_request = id_requests[message_id];
-        const member : GuildMember = (await interaction.guild.members.fetch())?.get(id_request['user_id']) as GuildMember;
+        const member: GuildMember = (await interaction.guild.members.fetch())?.get(id_request['user_id']) as GuildMember;
 
-        remove_id_message_interactions(interaction, message_id, `La demande d'identification a été validée par ${interaction.member?.user.username}`);
-        member.roles.add(role_id);
-        member.setNickname(`${id_request['firstname']} ${id_request['lastname']}`);
+        await member.roles.add(role_id);
 
-        if(member.roles.cache.get(RolesIds.Unverified_User_Role))
+        try {
+            await member.setNickname(`${id_request['firstname']} ${id_request['lastname']}`);
+        } catch { }
+        if (member.roles.cache.get(RolesIds.Unverified_User_Role))
             await member.roles.remove(RolesIds.Unverified_User_Role);
 
-        await remove_id_request(this.used_file, member, id_requests);
-        await member.send('Votre demande d\'identification a été acceptée !');
-        await interaction.reply({content: `La demande d'identification du membre ${id_request['lastname']} ${id_request['firstname']} a bien été acceptée`, ephemeral:true});
+        remove_id_request(this.used_file, member, id_requests);
+        member.send('Votre demande d\'identification a été acceptée !');
+        const reply_msg = await interaction.message.channel.send(`La demande d'identification du membre ${id_request['lastname']} ${id_request['firstname']} a bien été acceptée`);
+        setTimeout(async () => {
+            try {
+                await reply_msg.delete();
+            } catch { }
+        }, 5000);
+        interaction.message.delete();
+        await interaction.deferUpdate();
     }
 }
