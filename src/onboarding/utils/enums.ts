cdbed6a943e693f80e296b4a0a2596b815775076
Mecopi <mecopilive@gmail.com>
2023-01-20 20:03:29 +0100

    feat(common): update channels ids to feat with Cedric server ids

--- src/onboarding/utils/enums.ts
+++ src/onboarding/utils/enums.ts
@@ -1,9 +1,9 @@
 export enum ChannelsIds {
     Request_Identification_Channel = '',
-    Manage_Identification_Channel = '1065212511714017340',
+    Manage_Identification_Channel = '1066066276033757345',
 }
 
 export enum RolesIds {
-    Unverified_User_Role = '1065190525193441300',
-    CAP_Role = '1065224602613186600'
+    Unverified_User_Role = '1066066657648328704',
+    CAP_Role = '1066066811646390303'
 }
