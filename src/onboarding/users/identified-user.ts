403a5af77332958c4e664fa8e41b2dc44bbfe33d
NelsonPhilippe <philippenelson59@gmail.com>
2023-02-23 22:17:05 +0100

    feat(onboarding) : - add configuration class and initialize route for api - add classes for bases of bot - add utilities collections list and http class - Enumeration for routes

---
+++ src/onboarding/users/identified-user.ts
@@ -0,0 +1,32 @@
+import {User} from "./user";
+
+export class IdentifiedUser extends User{
+  private readonly _fistname: string;
+  private readonly _lastname: string;
+  private readonly _email: string;
+  private readonly _factory_name?: string;
+  constructor(id: number, pseudo: string, fistname: string, lastname: string, email: string, factory_name: string) {
+    super(id, pseudo);
+    this._fistname = fistname;
+    this._lastname = lastname;
+    this._email = email;
+    this._factory_name = factory_name;
+  }
+
+
+  get fistname(): string {
+    return this._fistname;
+  }
+
+  get lastname(): string {
+    return this._lastname;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get factory_name(): string | undefined {
+    return this._factory_name;
+  }
+}
