4bdaed4bf56883f10e2a77e02ca27d02200d78dc
Bourez Bastien <Bourezbastien@gmail.com>
2023-02-23 14:30:42 +0100

    build(gitignore): add build folder and files

---
+++ src/ticketing/index.ts
@@ -0,0 +1,28 @@
+import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
+import EventEmitter from "events";
+import * as dotenv from "dotenv";
+import command_handler from "./handlers/command_handler";
+import event_handler from "./handlers/event_handler";
+
+
+dotenv.config({path: "./folder_bot/.env"});
+process.setMaxListeners(0);
+EventEmitter.setMaxListeners(0);
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_ID = process.env.DISCORD_ID;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildInvites] });
+
+command_handler(client, DISCORD_TOKEN, DISCORD_ID);
+event_handler(client);
+
+client.on(Events.InteractionCreate, async (interaction : Interaction) => {
+    if(!interaction.isChatInputCommand()) return; 
+    const parsedClient : any = client;
+    const command = parsedClient.commands.get(interaction['commandName']);
+    command.execute(interaction);
+});
+
+
+client.login(DISCORD_TOKEN);
