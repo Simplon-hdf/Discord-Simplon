5f2d881375e919702f833c0e62eb44c7959a3554
Mecopi <mecopilive@gmail.com>
2023-03-07 15:25:10 +0100

    feat(handler): add component handler

---
+++ src/onboarding/handlers/components_handler.ts
@@ -0,0 +1,39 @@
+import { REST, Routes, Collection, Client } from "discord.js";
+import * as fs from "fs";
+import * as path from 'path';
+import * as dotenv from "dotenv";
+
+export default async (client: any) => {
+    dotenv.config();
+
+    const componentsFiles = getAllFiles('./build/components/');
+
+    function getAllFiles(dirPath: any, arrayOfFiles?: any) {
+
+        arrayOfFiles = arrayOfFiles || []
+        try {
+
+            const files = fs.readdirSync(dirPath)
+            files.forEach(function (file) {
+                if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+                    arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+                } else {
+                    arrayOfFiles.push(path.join(dirPath, '/', file));
+                }
+            })
+
+        } catch (error) {
+            console.log(error);
+
+        }
+        return arrayOfFiles
+    }
+
+    client.components = new Collection();
+
+    
+    for (const file of componentsFiles) {
+        const component = await import(`../../${file}`);
+        client.components.set(component.default.data.customId, component.default); // Link cmd name to complete module
+    }
+}
