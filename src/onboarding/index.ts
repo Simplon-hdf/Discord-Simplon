5bab6415bdc0d2674ed335d06a24f35029245d85
Mecopi <mecopilive@gmail.com>
2023-03-07 15:25:47 +0100

    feat(common): link component handler & try to get interaction handled

--- src/onboarding/index.ts
+++ src/onboarding/index.ts
@@ -3,8 +3,8 @@ import EventEmitter from "events";
 import * as dotenv from "dotenv";
 import command_handler from "./handlers/command_handler";
 import event_handler from "./handlers/event_handler";
+import components_handler from "./handlers/components_handler";
 import {HttpUtils} from "./utils/http";
-
 
 dotenv.config();
 process.setMaxListeners(0);
@@ -16,12 +16,18 @@ const DISCORD_ID = process.env.DISCORD_ID;
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildInvites] });
 command_handler(client, DISCORD_TOKEN, DISCORD_ID);
 event_handler(client);
+components_handler(client);
 
 client.on(Events.InteractionCreate, async (interaction : Interaction) => {
-    if(!interaction.isChatInputCommand()) return;
-    const parsedClient : any = client;
-    const command = parsedClient.commands.get(interaction['commandName']);
-    command.execute(interaction);
+    if(interaction.isChatInputCommand()) {
+      const parsedClient : any = client;
+      const command = parsedClient.commands.get(interaction['commandName']);
+      command.execute(interaction);
+    } else if (interaction.isButton()) {
+      const parsedClient : any = client;
+      for(const d of parsedClient.components)
+        console.log(d);
+    }
 });
 
 
