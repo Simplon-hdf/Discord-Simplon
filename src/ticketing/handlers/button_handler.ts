2a982511df2cebe164f2799733720698b2fc3bac
Bourez Bastien <Bourezbastien@gmail.com>
2023-03-08 16:16:38 +0100

    feat(ticketing): modification of architecture

--- src/ticketing/handlers/button_handler.ts
+++ src/ticketing/handlers/button_handler.ts
@@ -1,17 +1,19 @@
 import { readdirSync } from "fs";
-
 export default async (client: any) => {
-  const buttonFiles = readdirSync(
-    `${__dirname}/../../build/components/buttons/`
-  ).filter((file) => file.endsWith(".js"));
-  for (const file of buttonFiles) {
-    const button = (await import(`../components/buttons/${file}`)).default;
-    client.buttons.set(button.data.data.custom_id, button);
+  const buttonFolders = readdirSync(`${__dirname}/../../components/buttons`);
+  for (const category of buttonFolders) {
+    const buttonFiles = readdirSync(
+      `${__dirname}/../../build/components/buttons/${category}`
+    ).filter((file) => file.endsWith(".js"));
+    for (const file of buttonFiles) {
+      const button = (await import(`../components/buttons/${category}/${file}`)).default;
+      client.buttons.set(button.data.data.custom_id, button);
+    }
   }
   setTimeout(() => {
     console.log("---------------[ BUTTON(S) ]---------------")
-    console.table(buttonFiles)
-    console.log(`[BUTTON] => ${buttonFiles.length} Button(s) has been charged !`)
+    console.table(buttonFolders)
+    console.log(`[BUTTON] => ${buttonFolders.length} Button(s) has been charged !`)
     console.log(" ")
   }, 1000)
 };
