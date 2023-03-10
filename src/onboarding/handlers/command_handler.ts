e44d3b501c17c70d44e1f1da993073d5ff641233
Mecopi <mecopilive@gmail.com>
2023-03-10 23:48:13 +0100

    refactor(handler): global refactor of command handler

--- src/onboarding/handlers/command_handler.ts
+++ src/onboarding/handlers/command_handler.ts
@@ -4,57 +4,45 @@ import * as path from 'path';
 import * as dotenv from "dotenv";
 import { UtilsManager } from "../utils/UtilsManager";
 
-export default async (client: any, discord_token?: any, discord_client_id?: any) => {
+export default async (discord_token?: any, discord_client_id?: any) => {
   dotenv.config();
+
+  if (!discord_token || !discord_client_id) throw new Error('Discord id or token is undefined');
 
   const commandFiles = getAllFiles('./build/commands/');
 
   function getAllFiles(dirPath: any, arrayOfFiles?: any) {
-
     arrayOfFiles = arrayOfFiles || []
     try {
-      const files = fs.readdirSync(dirPath)
-      files.forEach(function (file) {
-        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-          arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-        } else {
-          arrayOfFiles.push(path.join("commands/", file));
-        }
-      })
-
+      const files = fs.readdirSync(dirPath);
+      files.forEach((file) => {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) { arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles); }
+        else { arrayOfFiles.push(path.join("commands/", file)); }
+      });
     } catch (error) {
       console.log(error);
-
     }
-    return arrayOfFiles
+    return arrayOfFiles;
   }
 
-  client.commands = new Collection();
-
   for (const file of commandFiles) {
-    const cmd_import = await import(`../${file}`);
     if ((file as string).includes("SlashCommand"))
       continue;
     try {
-      UtilsManager.add_command(new cmd_import.default); // Link cmd name to complete module
+      UtilsManager.add_command(new (await import(`../${file}`)).default); // Link cmd name to complete module
     } catch {
       console.log(`${file} command can't be load (must because it's not a constructor)`);
     }
   }
 
-  if (!discord_token || !discord_client_id) {
-    throw new Error('Discord id or token is undefined');
-
-  }
-  const rest = new REST({ version: '10' }).setToken(discord_token);
 
   (async () => {
     try {
       console.log('Started refreshing application (/) commands.');
 
       const serialized_commands = Array.from(UtilsManager.get_commands().values())
-        .map(slash_command => slash_command.data.toJSON());
-      await rest.put(Routes.applicationCommands(discord_client_id), { body: serialized_commands }); //Logging commands on RESTAPI (for each values in commands, get data.JSON() to register it
+        .map(slash_command => slash_command.get_data().toJSON());
+      await new REST({ version: '10' }).setToken(discord_token).put(Routes.applicationCommands(discord_client_id), { body: serialized_commands }); //Logging commands on RESTAPI (for each values in commands, get data.JSON() to register it
 
       console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
