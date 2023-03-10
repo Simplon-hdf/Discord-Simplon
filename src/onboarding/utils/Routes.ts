e46ea5fd7965c3d23c61a9fe14d0ad31503ee9b5
Mecopi <mecopilive@gmail.com>
2023-03-10 13:44:54 +0100

    feat(utils): add user utils functions, correct http utils post func, add roads for api

--- src/onboarding/utils/Routes.ts
+++ src/onboarding/utils/Routes.ts
@@ -1,5 +1,9 @@
 export enum Routes {
   GET_GUILD_CONFIG = '/api/guilds/config',
   GET_GUILD = '/api/guilds/:id',
-  CREATE_GUILD = '/api/guilds/create'
+  CREATE_GUILD = '/api/guilds/register',
+  GET_COURSES_BY_GUILD_UUID = '/api/courses/guilds/:uuid',
+  GET_ONGOING_PROMOS_BY_GUILD_UUID = '/api/promo/guild/:uuid/state/true',
+  GET_ROLES_BY_GUILD_UUID = '/api/roles/guilds/:uuid',
+  GET_PROMOS_BY_GUILD_UUID = '/api/promo/guild/:uuid'
 }
