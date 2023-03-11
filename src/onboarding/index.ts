f54d6da94a3d7f0908195bbf99bf90f19fddee5c
Mecopi <mecopilive@gmail.com>
2023-03-11 01:59:59 +0100

    feat(common): refactor index.ts and feat interactionn dispatch to handle component interactions

--- src/onboarding/index.ts
+++ src/onboarding/index.ts
@@ -1,10 +1,6 @@
 import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
 import EventEmitter from "events";
 import * as dotenv from "dotenv";
-import command_handler from "./handlers/command_handler";
-import event_handler from "./handlers/event_handler";
-import components_handler from "./handlers/components_handler";
-import { ProcedureManager } from "./utils/procedures/ProcedureManager";
 import { UtilsManager } from "./utils/UtilsManager";
 
 dotenv.config();
@@ -15,19 +11,11 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const DISCORD_ID = process.env.DISCORD_ID;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildInvites] });
-command_handler(DISCORD_TOKEN, DISCORD_ID);
-event_handler(client);
-components_handler(client);
-(client as any).procedureManager = new ProcedureManager();
+UtilsManager.init(client, DISCORD_TOKEN, DISCORD_ID);
 
 client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-  const parsedClient: any = client;
-  if (interaction.isChatInputCommand()) {
-    UtilsManager.get_command(interaction['commandName'])?.execute(interaction);
-  } else {
-    const castedInteraction: any = interaction;
-    parsedClient.components.get(castedInteraction['customId']).execute(interaction);
-  }
+  if (interaction.isChatInputCommand()) { UtilsManager.get_command(interaction['commandName'])?.execute(interaction); } 
+  else { UtilsManager.get_component((interaction as any)['customId'])?.execute(interaction); }
 });
 
 client.login(DISCORD_TOKEN);
