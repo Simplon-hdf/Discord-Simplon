9b335707b9f5c766158c970a962e95bad6c9c5b3
Mecopi <mecopilive@gmail.com>
2023-03-11 02:02:04 +0100

    feat(common): rework component handler and refactor

--- src/onboarding/handlers/components_handler.ts
+++ src/onboarding/handlers/components_handler.ts
@@ -1,40 +1,32 @@
-import { REST, Routes, Collection, Client } from "discord.js";
 import * as fs from "fs";
 import * as path from 'path';
 import * as dotenv from "dotenv";
+import CustomComponent from "../components/CustomComponent";
+import { UtilsManager } from "../utils/UtilsManager";
 
-export default async (client: any) => {
-    dotenv.config();
+export default async () => {
+  dotenv.config();
 
-    const componentsFiles = getAllFiles('./build/components/');
+  const componentsFiles: string[] = getAllFiles('./build/components/');
 
-    function getAllFiles(dirPath: any, arrayOfFiles?: any) {
-
-        arrayOfFiles = arrayOfFiles || []
-        try {
-
-            const files = fs.readdirSync(dirPath)
-            files.forEach(function (file) {
-                if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-                    arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-                } else {
-                    arrayOfFiles.push(path.join(dirPath, '/', file));
-                }
-            })
-
-        } catch (error) {
-            console.log(error);
-
-        }
-        return arrayOfFiles
+  function getAllFiles(dirPath: string, arrayOfFiles: string[] = []) {
+    arrayOfFiles = arrayOfFiles || []
+    try {
+      const files = fs.readdirSync(dirPath);
+      files.forEach(function (file) {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) { arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles); }
+        else { arrayOfFiles.push(path.join(dirPath, '/', file)); }
+      });
+    } catch (error) {
+      console.log(error);
     }
+    return arrayOfFiles;
+  }
 
-    client.components = new Collection();
-
-    
-    for (const file of componentsFiles) {
-        const component = await import(`../../${file}`);
-        if(component.default.data.data.custom_id !== undefined)
-          client.components.set(component.default.data.data.custom_id, component.default); // Link cmd name to complete module
-    }
+  for (const file of componentsFiles) {
+    if (file.includes('CustomComponent'))
+      continue;
+    const component: CustomComponent = new (await import(`../../${file}`)).default as CustomComponent;
+    UtilsManager.add_component(component);
+  }
 }
