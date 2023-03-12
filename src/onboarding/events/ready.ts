2cc91c0b95bffbf2b02bfed2ae4d73ce2c892d4b
Mecopi <mecopilive@gmail.com>
2023-03-12 13:57:34 +0100

    fix(files): clean working tree

--- src/onboarding/events/ready.ts
+++ src/onboarding/events/ready.ts
@@ -322,11 +322,3 @@ function getDiscordChannels(guild: DiscordGuild) {
       channel.parent?.type !== ChannelType.GuildText,
   );
 }
-
-export default {
-    name : Events.ClientReady,
-    once: true,
-    execute(client : Client) {
-        console.log("Ready! Logged in as " + client.user?.tag);
-    }
-}
